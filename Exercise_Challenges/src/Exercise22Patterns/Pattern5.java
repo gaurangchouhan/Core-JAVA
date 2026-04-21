c7fe2aacae55735e1d1a26730d752f32f19297b5
gaurangchouhan <gaurangchouhan@gmail.com>
2026-04-21 21:54:07 +0530

    changes & add on codes

--- Exercise_Challenges/src/Exercise22Patterns/Pattern5.java
+++ Exercise_Challenges/src/Exercise22Patterns/Pattern5.java
@@ -2,10 +2,16 @@ package Exercise22Patterns;
 
 public class Pattern5 {
     void pattern(){
-
+        for (int i = 0; i < 6; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-
+        Pattern5 p5 = new Pattern5();
+        p5.pattern();
     }
 }
